600c0f58906083e7f9ec4797caf191ac3cb445da
ChrisWuUNM <chris080695@gmail.com>
2017-04-12 01:15:13 -0600

    Pushing up my desktop

---
+++ 3d_Tic_Tac_Toe/Assets/Scripts/Manager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour {
+
+    public static Manager manager = null;
+    public bool turn = true; //X is first
+    public char[,] board = new char[3,3];
+
+    void Awake()
+    {
+        if (manager == null)
+        {
+            manager = this;
+        }
+        else if(manager != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
